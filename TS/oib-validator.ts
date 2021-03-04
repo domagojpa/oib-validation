c63a89d7d4ef18e32daa565e1548a7eb2caf488e
Hrvoje Hudo <hrvojeh@microsoft.com>
2021-03-04 22:33:45 +0100

    Loop refactor

--- TS/oib-validator.ts
+++ TS/oib-validator.ts
@@ -3,13 +3,13 @@ const isValidOIB = (oib:string): boolean => {
         return false;
 
     let a = 10;
-    for (let idx = 0; idx < 10; idx++) {
-        a = a + parseInt(oib.substr(idx, 1), 10);
-        a = a % 10;
+    for(const digit of oib.substring(0, 10)) {
+        a += parseInt(digit);
+        a %= 10;
         if (a === 0) 
             a = 10;
         a *= 2;
-        a = a % 11;
+        a %= 11;
     }
 
     let kontrolni = 11 - a;
