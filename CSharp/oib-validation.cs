763349f507880e3772a875111d79a58d558bd3c9
Hrvoje Hudo <hrvojeh@microsoft.com>
2021-03-04 10:34:49 +0100

    Added input validation and try/catch

--- CSharp/oib-validation.cs
+++ CSharp/oib-validation.cs
@@ -1,30 +1,43 @@
 ﻿using System;
-using System.Web;
 
 /// <summary>
 /// Summary description for oib
 /// </summary>
 public class oib
 {
-    public static bool CheckOIB(string oib)
-    {
-        if (oib.Length != 11) return false;
+	public static bool CheckOIB(string oib)
+	{
+		if (string.IsNullOrEmpty(oib) || oib.Length != 11) 
+			return false;
 
-        long b;
-        if (!long.TryParse(oib, out b)) return false;
+		if (!long.TryParse(oib, out _)) 
+			return false;
 
-        int a = 10;
-        for (int i = 0; i < 10; i++)
-        {
-            a = a + Convert.ToInt32(oib.Substring(i, 1));
-            a = a % 10;
-            if (a == 0) a = 10;
-            a *= 2;
-            a = a % 11;
-        }
-        int kontrolni = 11 - a;
-        if (kontrolni == 10) kontrolni = 0;
+		try
+		{
+			var a = 10;
+			for (var i = 0; i < 10; i++)
+			{
+				a = a + Convert.ToInt32(oib.Substring(i, 1));
+				a = a % 10;
 
-        return kontrolni == Convert.ToInt32(oib.Substring(10, 1));
-    }
+				if (a == 0) 
+					a = 10;
+
+				a *= 2;
+				a = a % 11;
+			}
+
+			var kontrolni = 11 - a;
+
+			if (kontrolni == 10) 
+				kontrolni = 0;
+
+			return kontrolni == Convert.ToInt32(oib.Substring(10, 1));
+		}
+		catch (Exception)
+		{
+			return false;
+		}
+	}
 }
