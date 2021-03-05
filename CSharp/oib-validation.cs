f530ef5d1d42740754e73b6b9ace2f663c8a55d8
apozgaj <andrijap@ebe-inc.com>
2021-03-05 21:40:27 +0100

    CSharp - merge master and add span

--- CSharp/oib-validation.cs
+++ CSharp/oib-validation.cs
@@ -8,17 +8,19 @@ public class Validators
 		if (string.IsNullOrEmpty(oib) || !Regex.IsMatch(oib, "^[0-9]{11}$")) 
 			return false;
 
+        var oibSpan = oib.AsSpan();
+
 		var a = 10;
 		for (var i = 0; i < 10; i++)
 		{
-			a = a + Convert.ToInt32(oib.Substring(i, 1));
-			a = a % 10;
+			a += int.Parse(oibSpan.Slice(i, 1));
+			a %= 10;
 
 			if (a == 0) 
 				a = 10;
 
 			a *= 2;
-			a = a % 11;
+			a %= 11;
 		}
 
 		var kontrolni = 11 - a;
@@ -26,6 +28,6 @@ public class Validators
 		if (kontrolni == 10) 
 			kontrolni = 0;
 
-		return kontrolni == Convert.ToInt32(oib.Substring(10, 1));
+		return kontrolni == int.Parse(oibSpan.Slice(10, 1));
 	}
 }
